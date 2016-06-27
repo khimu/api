137ca2c332563c8d6effab35fec597a6d325cdbe
khimung <kim2kim@gmail.com>
2016-06-27 01:43:45 -0700

    Update transactions job to use different package name

---
+++ transaction-score/copyScript.sh
@@ -0,0 +1,5 @@
+scp target/match-executable.jar root@107.170.234.144:/opt/match/
+scp target/match-executable.jar root@107.170.234.144:/opt/cache/
+scp target/match-executable.jar root@107.170.234.144:/opt/score/
+scp target/match-executable.jar root@107.170.234.144:/opt/flatten_score/
+
